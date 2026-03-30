671795317fc9556feb2f5d7ce8df033370b17031
annseojin <hjsj1226@naver.com>
2026-03-30 18:15:38 +0900

    fix /auth/app/models/users.py

--- services/auth/app/models/user.py
+++ services/auth/app/models/user.py
@@ -5,5 +5,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password = Column(String)
+    
+    username = Column(String(50), unique=True, index=True, nullable=False)
+    password = Column(String(255), nullable=False)
