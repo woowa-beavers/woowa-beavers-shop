4e34bc71f3c76675a8457163c0117186b63a9139
Jiwon Yang <a01031275246@gmail.com>
2026-04-01 13:17:09 +0900

    refactor

--- services/order/app/routers/wallet.py
+++ services/order/app/routers/wallet.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.services.wallet_service import WalletService
 
-router = APIRouter(prefix="/api/order/wallets", tags=["Wallet"])
+router = APIRouter(prefix="/api/orders/wallets", tags=["Wallet"])
 
 @router.post("/{user_id}") # [서진] 회원가입 하면 정보를 받아와서 지갑 생성해주는 api
 async def create_user_wallet(user_id: str, db: Session = Depends(get_db)):
